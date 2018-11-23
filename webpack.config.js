6d89798534a451b8c93fa3c7798ced6d2574a5d5
Tarek Raafat <tarek.m.raafat@gmail.com>
2018-11-23 09:23:23 +0200

    Fixed the library name in the webpack.config.js file.

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
 	output: {
 		path: path.resolve(__dirname, "dist"),
 		filename: "js/autoComplete.js",
-		library: "autoComplete.js",
+		library: "autoComplete",
 		libraryTarget: "umd"
 	},
 	devServer: {
