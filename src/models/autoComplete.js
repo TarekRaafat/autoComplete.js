9e66f06e7ba21173d9b83705c0cae193a53707cf
Tarek <tarek.m.raafat@gmail.com>
2018-12-29 00:36:15 +0000

    Update autoComplete.js
    The API of data attribute value change has to be removed, otherwise if users changed values result selection mechanism will break.

--- src/models/autoComplete.js
+++ src/models/autoComplete.js
@@ -26,11 +26,6 @@ export default class autoComplete {
 		this.maxResults = config.maxResults || 5;
 		// Highlighting matching results
 		this.highlight = config.highlight || false;
-		// Assign data attribute tag & value if set in object
-		this.dataAttribute = {
-			tag: config.dataAttribute ? config.dataAttribute.tag : "autocomplete",
-			value: config.dataAttribute ? config.dataAttribute.value : ""
-		};
 		// Action function on result selection
 		this.onSelection = config.onSelection;
 		// Starts the app Enigne
