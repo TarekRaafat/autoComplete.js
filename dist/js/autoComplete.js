48f9e01a88135adf23d64905c1143d751ad95411
Tarek Raafat <tarek.m.raafat@gmail.com>
2019-04-28 06:59:58 +0200

    New Build & Package Update

--- dist/js/autoComplete.js
+++ dist/js/autoComplete.js
@@ -214,8 +214,8 @@
                 _iteratorError = err;
               } finally {
                 try {
-                  if (!_iteratorNormalCompletion && _iterator.return != null) {
-                    _iterator.return();
+                  if (!_iteratorNormalCompletion && _iterator["return"] != null) {
+                    _iterator["return"]();
                   }
                 } finally {
                   if (_didIteratorError) {
