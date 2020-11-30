201db7c3c8a92d71b8c35c6415290aeb2bba384d
Tarek Raafat <tarek.m.raafat@gmail.com>
2020-11-30 20:41:01 +0200

    Demo Update

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,29 +1,29 @@
 // // autoComplete.js input eventListener on connect event
-// document.querySelector("#autoComplete").addEventListener("connect", function (event) {
+// document.querySelector("#autoCompleteJS").addEventListener("connect", function (event) {
 //   console.log(event);
 // });
 // // autoComplete.js input eventListener on initialization event
-// document.querySelector("#autoComplete").addEventListener("init", function (event) {
+// document.querySelector("#autoCompleteJS").addEventListener("init", function (event) {
 //   console.log(event);
 // });
 // // autoComplete.js input eventListener on data response event
-// document.querySelector("#autoComplete").addEventListener("fetch", function (event) {
+// document.querySelector("#autoCompleteJS").addEventListener("fetch", function (event) {
 //   console.log(event);
 // });
 // // autoComplete.js input eventListener on search results event
-// document.querySelector("#autoComplete").addEventListener("results", function (event) {
+// document.querySelector("#autoCompleteJS").addEventListener("results", function (event) {
 //   console.log(event);
 // });
 // // autoComplete.js input eventListener on post results list rendering event
-// document.querySelector("#autoComplete").addEventListener("rendered", function (event) {
+// document.querySelector("#autoCompleteJS").addEventListener("rendered", function (event) {
 //   console.log(event);
 // });
-// // autoComplete.js input eventListener on results list navigation
-// document.querySelector("#autoComplete").addEventListener("navigation", function (event) {
-//   console.log(event);
-// });
+// autoComplete.js input eventListener on results list navigation
+document.querySelector("#autoCompleteJS").addEventListener("navigation", function (event) {
+  console.log(event.detail);
+});
 // // autoComplete.js input eventListener on post un-initialization event
-// document.querySelector("#autoComplete").addEventListener("unInit", function (event) {
+// document.querySelector("#autoCompleteJS").addEventListener("unInit", function (event) {
 //   console.log(event);
 // });
 
