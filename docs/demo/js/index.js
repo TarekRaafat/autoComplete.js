c121cee274e56ec7eac3b0aabc28f8ac5a9f418d
Tarek Raafat <tarek.m.raafat@gmail.com>
2020-12-04 16:46:52 +0200

    v8.0 Merge

--- docs/demo/js/index.js
+++ docs/demo/js/index.js
@@ -10,7 +10,7 @@ const autoCompleteJS = new autoComplete({
       // Loading placeholder text
       document.querySelector("#autoComplete").setAttribute("placeholder", "Loading...");
       // Fetch External Data Source
-      const source = await fetch("./db/generic.json");
+      const source = await fetch("../demo/db/generic.json");
       const data = await source.json();
       // Returns Fetched data
       return data;
