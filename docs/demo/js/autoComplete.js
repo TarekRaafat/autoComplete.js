5a134363af1f6136f34536d94e0294e91024822c
Tarek Raafat <tarek.m.raafat@gmail.com>
2021-05-30 10:20:42 +0200

    Removed: Live Reload

--- docs/demo/js/autoComplete.js
+++ docs/demo/js/autoComplete.js
@@ -1,5 +1,3 @@
-
-(function(l, r) { if (l.getElementById('livereloadscript')) return; r = l.createElement('script'); r.async = 1; r.src = '//' + (window.location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1'; r.id = 'livereloadscript'; l.getElementsByTagName('head')[0].appendChild(r) })(window.document);
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
   typeof define === 'function' && define.amd ? define(factory) :
