fe0202b0b1e7047b9b0fc6f406a2a3038e6aae65
folk <folk@folk.wtf>
2025-07-09 13:17:05 +0200

    Fix demo. Most of the changes here are because index.html got formatted by prettier when I saved.

--- docs/demo/js/index.js
+++ docs/demo/js/index.js
@@ -1,134 +1,144 @@
-// The autoComplete.js Engine instance creator
-const autoCompleteJS = new autoComplete({
-  data: {
-    src: async () => {
-      try {
-        // Loading placeholder text
-        document.getElementById("autoComplete").setAttribute("placeholder", "Loading...");
-        // Fetch External Data Source
-        const source = await fetch("./db/generic.json");
-        const data = await source.json();
-        // Post Loading placeholder text
-        document.getElementById("autoComplete").setAttribute("placeholder", autoCompleteJS.placeHolder);
-        // Returns Fetched data
-        return data;
-      } catch (error) {
-        return error;
-      }
-    },
-    keys: ["food", "cities", "animals"],
-    cache: true,
-    filter: (list) => {
-      // Filter duplicates
-      // incase of multiple data keys usage
-      const filteredResults = Array.from(new Set(list.map((value) => value.match))).map((food) => {
-        return list.find((value) => value.match === food);
-      });
+import autoComplete from "./autoComplete.js";
 
-      return filteredResults;
+async function init() {
+  // The autoComplete.js Engine instance creator
+  const autoCompleteJS = new autoComplete({
+    data: {
+      src: async () => {
+        try {
+          // Loading placeholder text
+          document.getElementById("autoComplete").setAttribute("placeholder", "Loading...");
+          // Fetch External Data Source
+          const source = await fetch("./db/generic.json");
+          const data = await source.json();
+          // Post Loading placeholder text
+          document.getElementById("autoComplete").setAttribute("placeholder", autoCompleteJS.placeHolder);
+          // Returns Fetched data
+          return data;
+        } catch (error) {
+          return error;
+        }
+      },
+      keys: ["food", "cities", "animals"],
+      cache: true,
+      filter: (list) => {
+        // Filter duplicates
+        // incase of multiple data keys usage
+        const filteredResults = Array.from(new Set(list.map((value) => value.match))).map((food) => {
+          return list.find((value) => value.match === food);
+        });
+
+        return filteredResults;
+      },
     },
-  },
-  placeHolder: "Search for Food & Drinks!",
-  resultsList: {
-    element: (list, data) => {
-      const info = document.createElement("p");
-      if (data.results.length) {
-        info.innerHTML = `Displaying <strong>${data.results.length}</strong> out of <strong>${data.matches.length}</strong> results`;
-      } else {
-        info.innerHTML = `Found <strong>${data.matches.length}</strong> matching results for <strong>"${data.query}"</strong>`;
-      }
-      list.prepend(info);
+    placeHolder: "Search for Food & Drinks!",
+    resultsList: {
+      element: (list, data) => {
+        const info = document.createElement("p");
+        if (data.results.length) {
+          info.innerHTML = `Displaying <strong>${data.results.length}</strong> out of <strong>${data.matches.length}</strong> results`;
+        } else {
+          info.innerHTML = `Found <strong>${data.matches.length}</strong> matching results for <strong>"${data.query}"</strong>`;
+        }
+        list.prepend(info);
+      },
+      noResults: true,
+      maxResults: 15,
+      tabSelect: true,
     },
-    noResults: true,
-    maxResults: 15,
-    tabSelect: true,
-  },
-  resultItem: {
-    element: (item, data) => {
-      // Modify Results Item Style
-      item.style = "display: flex; justify-content: space-between;";
-      // Modify Results Item Content
-      item.innerHTML = `
+    resultItem: {
+      element: (item, data) => {
+        // Modify Results Item Style
+        item.style = "display: flex; justify-content: space-between;";
+        // Modify Results Item Content
+        item.innerHTML = `
       <span style="text-overflow: ellipsis; white-space: nowrap; overflow: hidden;">
         ${data.match}
       </span>
       <span style="display: flex; align-items: center; font-size: 13px; font-weight: 100; text-transform: uppercase; color: rgba(0,0,0,.2);">
         ${data.key}
       </span>`;
-    },
-    highlight: true,
-  },
-  events: {
-    input: {
-      focus() {
-        if (autoCompleteJS.input.value.length) autoCompleteJS.start();
       },
-      selection(event) {
-        const feedback = event.detail;
-        autoCompleteJS.input.blur();
-        // Prepare User's Selected Value
-        const selection = feedback.selection.value[feedback.selection.key];
-        // Render selected choice to selection div
-        document.querySelector(".selection").innerHTML = selection;
-        // Replace Input value with the selected value
-        autoCompleteJS.input.value = selection;
-        // Console log autoComplete data feedback
-        console.log(feedback);
+      highlight: true,
+    },
+    events: {
+      input: {
+        focus() {
+          if (autoCompleteJS.input.value.length) autoCompleteJS.start();
+        },
+        selection(event) {
+          const feedback = event.detail;
+          autoCompleteJS.input.blur();
+          // Prepare User's Selected Value
+          const selection = feedback.selection.value[feedback.selection.key];
+          // Render selected choice to selection div
+          document.querySelector(".selection").innerHTML = selection;
+          // Replace Input value with the selected value
+          autoCompleteJS.input.value = selection;
+          // Console log autoComplete data feedback
+          console.log(feedback);
+        },
       },
     },
-  },
-});
+  });
 
-// Toggle Search Engine Type/Mode
-document.querySelector(".toggler").addEventListener("click", () => {
-  // Holds the toggle button selection/alignment
-  const toggle = document.querySelector(".toggle").style.justifyContent;
+  // Toggle Search Engine Type/Mode
+  document.querySelector(".toggler").addEventListener("click", () => {
+    // Holds the toggle button selection/alignment
+    const toggle = document.querySelector(".toggle").style.justifyContent;
 
-  if (toggle === "flex-start" || toggle === "") {
-    // Set Search Engine mode to Loose
-    document.querySelector(".toggle").style.justifyContent = "flex-end";
-    document.querySelector(".toggler").innerHTML = "Loose";
-    autoCompleteJS.searchEngine = "loose";
-  } else {
-    // Set Search Engine mode to Strict
-    document.querySelector(".toggle").style.justifyContent = "flex-start";
-    document.querySelector(".toggler").innerHTML = "Strict";
-    autoCompleteJS.searchEngine = "strict";
-  }
-});
-
-// Blur/unBlur page elements
-const action = (action) => {
-  const github = document.querySelector(".github-corner");
-  const title = document.querySelector("h1");
-  const mode = document.querySelector(".mode");
-  const selection = document.querySelector(".selection");
-  const footer = document.querySelector(".footer");
-
-  if (action === "dim") {
-    github.style.opacity = 1;
-    title.style.opacity = 1;
-    mode.style.opacity = 1;
-    selection.style.opacity = 1;
-    footer.style.opacity = 1;
-  } else {
-    github.style.opacity = 0.1;
-    title.style.opacity = 0.3;
-    mode.style.opacity = 0.2;
-    selection.style.opacity = 0.1;
-    footer.style.opacity = 0.1;
-  }
-};
-
-// Blur/unBlur page elements on input focus
-["focus", "blur"].forEach((eventType) => {
-  autoCompleteJS.input.addEventListener(eventType, () => {
-    // Blur page elements
-    if (eventType === "blur") {
-      action("dim");
-    } else if (eventType === "focus") {
-      // unBlur page elements
-      action("light");
+    if (toggle === "flex-start" || toggle === "") {
+      // Set Search Engine mode to Loose
+      document.querySelector(".toggle").style.justifyContent = "flex-end";
+      document.querySelector(".toggler").innerHTML = "Loose";
+      autoCompleteJS.searchEngine = "loose";
+    } else {
+      // Set Search Engine mode to Strict
+      document.querySelector(".toggle").style.justifyContent = "flex-start";
+      document.querySelector(".toggler").innerHTML = "Strict";
+      autoCompleteJS.searchEngine = "strict";
     }
   });
-});
+
+  // Blur/unBlur page elements
+  const action = (action) => {
+    const github = document.querySelector(".github-corner");
+    const title = document.querySelector("h1");
+    const mode = document.querySelector(".mode");
+    const selection = document.querySelector(".selection");
+    const footer = document.querySelector(".footer");
+
+    if (action === "dim") {
+      github.style.opacity = 1;
+      title.style.opacity = 1;
+      mode.style.opacity = 1;
+      selection.style.opacity = 1;
+      footer.style.opacity = 1;
+    } else {
+      github.style.opacity = 0.1;
+      title.style.opacity = 0.3;
+      mode.style.opacity = 0.2;
+      selection.style.opacity = 0.1;
+      footer.style.opacity = 0.1;
+    }
+  };
+
+  // Blur/unBlur page elements on input focus
+  ["focus", "blur"].forEach((eventType) => {
+    autoCompleteJS.input.addEventListener(eventType, () => {
+      // Blur page elements
+      if (eventType === "blur") {
+        action("dim");
+      } else if (eventType === "focus") {
+        // unBlur page elements
+        action("light");
+      }
+    });
+  });
+}
+
+if (document.readyState === "loading") {
+  document.addEventListener("DOMContentLoaded", init);
+} else {
+  init();
+}
